168b7642ddf28cf3a56bff4d6174b164a3c95343
evan yung <yqq_1993@163.com>
2021-09-09 18:02:09 +0800

    grapfql

--- app.ts
+++ app.ts
@@ -3,6 +3,8 @@ import { logger } from './deps.ts'
 
 import { Static, ErrorCatcher } from './middlewares/mod.ts'
 
+import { GraphQLService } from './graphql.ts'
+
 import routerApi from './router/mod.ts'
 
 const app = new Application()
@@ -24,6 +26,8 @@ app.use(Static)
 app.use(ErrorCatcher)
 
 router.use(routerApi.routes())
+
+app.use(GraphQLService.routes(), GraphQLService.allowedMethods())
 
 app.use(router.routes()).use(router.allowedMethods())
 
--- server.ts
+++ server.ts
@@ -1,11 +1,8 @@
 import app from './app.ts'
 import { colors } from './deps.ts'
-import { GraphQLService } from './graphql.ts'
 
 app.addEventListener('listen', ({ hostname, port, secure }) => {
   console.log(`Listening on: ${colors.green(`${secure ? 'https://' : 'http://'}${hostname ?? 'localhost'}:${port}`)}`)
 })
-
-app.use(GraphQLService.routes(), GraphQLService.allowedMethods())
 
 await app.listen({ hostname: '127.0.0.1', port: 8001 })
