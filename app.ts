7302da0bacfcdc8c2a01c7275a022e74228c8e1d
evan yung <yqq_1993@163.com>
2021-09-09 14:14:40 +0800

    app

---
+++ app.ts
@@ -0,0 +1,30 @@
+import { Application, Router } from './deps.ts'
+import { logger } from './deps.ts'
+
+import { Static, ErrorCatcher } from './middlewares/mod.ts'
+
+import routerApi from './router/mod.ts'
+
+const app = new Application()
+const router = new Router({ prefix: '/deno' })
+
+app.addEventListener('error', (evt) => {
+  // Will log the thrown error to the console.
+  console.log(evt.error.message)
+})
+
+// logger
+app.use(logger.logger)
+app.use(logger.responseTime)
+
+// static
+app.use(Static)
+
+// error catcher
+app.use(ErrorCatcher)
+
+router.use(routerApi.routes())
+
+app.use(router.routes()).use(router.allowedMethods())
+
+export default app
--- server.ts
+++ server.ts
@@ -1,34 +1,8 @@
-import { Application, Router } from './deps.ts'
-import { logger } from './deps.ts'
+import app from './app.ts'
 import { colors } from './dev_deps.ts'
 
-import { Static, ErrorCatcher } from './middlewares/mod.ts'
-
-import routerApi from './router/mod.ts'
-
-const app = new Application()
-const router = new Router({ prefix: '/deno' })
 app.addEventListener('listen', ({ hostname, port, secure }) => {
   console.log(`Listening on: ${colors.green(`${secure ? 'https://' : 'http://'}${hostname ?? 'localhost'}:${port}`)}`)
 })
-
-app.addEventListener('error', (evt) => {
-  // Will log the thrown error to the console.
-  console.log(evt.error.message)
-})
-
-// logger
-app.use(logger.logger)
-app.use(logger.responseTime)
-
-// static
-app.use(Static)
-
-// error catcher
-app.use(ErrorCatcher)
-
-router.use(routerApi.routes())
-
-app.use(router.routes()).use(router.allowedMethods())
 
 await app.listen({ hostname: '127.0.0.1', port: 8001 })
