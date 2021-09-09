9aff680593624421f0a7623192caaecaa98a9842
evan yung <yqq_1993@163.com>
2021-09-09 13:33:04 +0800

    logger

--- middlewares/mod.ts
+++ middlewares/mod.ts
@@ -1,15 +1,15 @@
 import { Middleware, send } from '../deps.ts'
-import { chalkin } from '../deps.ts'
+// import { chalkin } from '../deps.ts'
 import { responseBody } from '../types/mod.d.ts'
 
-export const Logger: Middleware = async (ctx, next) => {
-  await next()
-  const rt = ctx.response.headers.get('X-Response-Time')
-  const { method, url } = ctx.request
-  const { status } = ctx.response
-  const color = status === 200 ? 'green' : 'red'
-  console.log(`${chalkin.bold.green(method)} ${chalkin[color](status)} ${url} - ${chalkin.blue(rt)}`)
-}
+// export const Logger: Middleware = async (ctx, next) => {
+//   await next()
+//   const rt = ctx.response.headers.get('X-Response-Time')
+//   const { method, url } = ctx.request
+//   const { status } = ctx.response
+//   const color = status === 200 ? 'green' : 'red'
+//   console.log(`${chalkin.bold.green(method)} ${chalkin[color](status)} ${url} - ${chalkin.blue(rt)}`)
+// }
 
 export const Timing: Middleware = async (ctx, next) => {
   const start = Date.now()
